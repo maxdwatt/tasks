8df7229335bfef51b3836ef78a7192e96e00a84c
maxdwatt <79388280+maxdwatt@users.noreply.github.com>
2022-09-07 22:52:45 -0400

    task3 basic html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,53 @@
 import React from "react";
 import "./App.css";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                <h1>My Favorite Foods Of All Time for CISC275</h1>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
             </header>
-            <p>Hello World by Maxyne Watkins for CISC275.</p>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                border: "1px solid red",
+                                padding: "10px",
+                                width: "10px",
+                                height: "10px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                        <ul>
+                            <li>Mexican</li>
+                            <li>Spinach and Ricotta Ravioli</li>
+                            <li>Pastelles</li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                border: "1px solid red",
+                                padding: "10px",
+                                width: "10px",
+                                height: "10px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                        <img
+                            src={require("./watercolor-cute-avocado-cartoon-character_283146-768.jpg")}
+                            width="500"
+                            height="500"
+                            alt="Cute Avocado"
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
